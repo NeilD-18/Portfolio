a354ffffbc7dbe1d9c77d4dcd0a5d13655284963
Neil Daterao <ndaterao@gmail.com>
2025-01-13 14:40:32 -0500

    Added changes on main portfolio page to fetch experiences from database. Portal and main page are now linked for about bio, about images, and all experience categories (dates, name, picture ) etc...

--- client/src/pages/main/Experience.jsx
+++ client/src/pages/main/Experience.jsx
@@ -4,73 +4,31 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
-
 import "react-vertical-timeline-component/style.min.css";
-
 import { styles } from "../../styles";
-
 import { SectionWrapper } from "../../hoc";
-
 import { textVariant } from "../../utils/motion";
+import { useState, useEffect } from "react";
+import { handleFetchExperiences } from "./handlers/experienceSectionHandlers";
+import { format } from 'date-fns'
 
 
+const ExperienceCard = ({ experience, index }) => {
+  // Adjust dates to fix the month issue
+  const adjustedStartDate = new Date(
+    new Date(experience.dateRange.startDate).getFullYear(),
+    new Date(experience.dateRange.startDate).getMonth() + 1, 
+    1 // Set the day to 1 to avoid overflow
+  );
 
-const experiences = [
-  {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: '/Neil_Logo.svg',
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: '/Neil_Logo.svg',
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: '/Neil_Logo.svg',
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: '/Neil_Logo.svg',
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-];
+  const adjustedEndDate = new Date(
+    new Date(experience.dateRange.endDate).getFullYear(),
+    new Date(experience.dateRange.endDate).getMonth() + 1, 
+    1 // Set the day to 1 to avoid overflow
+  );
 
-const ExperienceCard = ({ experience }) => {
+  const formattedDate = `${format(adjustedStartDate, 'MMM yyyy')} - ${format(adjustedEndDate, 'MMM yyyy')}`;
+
   return (
     <VerticalTimelineElement
       contentStyle={{
@@ -78,30 +36,32 @@ const ExperienceCard = ({ experience }) => {
         color: "#fff",
       }}
       contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-      date={experience.date}
-      iconStyle={{ background: experience.iconBg }}
+      date={formattedDate}
+      iconStyle={{
+        background: index % 2 === 0 ? "#E6DEDD" : "#383E56",
+      }}
       icon={
-        <div className='flex justify-center items-center w-full h-full'>
+        <div className='flex justify-center items-center w-full h-full overflow-hidden rounded-full'>
           <img
-            src={experience.icon}
-            alt={experience.company_name}
-            className='w-[60%] h-[60%] object-contain'
+            src={experience.companyPicture}
+            alt={experience.companyName}
+            className='w-full h-full object-cover'
           />
         </div>
       }
     >
       <div>
-        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <h3 className='text-white text-[24px] font-bold'>{experience.role}</h3>
         <p
           className='text-secondary text-[16px] font-semibold'
           style={{ margin: 0 }}
         >
-          {experience.company_name}
+          {experience.companyName}
         </p>
       </div>
 
       <ul className='mt-5 list-disc ml-5 space-y-2'>
-        {experience.points.map((point, index) => (
+        {experience.responsibilities.map((point, index) => (
           <li
             key={`experience-point-${index}`}
             className='text-white-100 text-[14px] pl-1 tracking-wider'
@@ -115,6 +75,13 @@ const ExperienceCard = ({ experience }) => {
 };
 
 const Experience = () => {
+  
+  const [experiences, setExperiences] = useState([])
+  
+  useEffect(() => { 
+    handleFetchExperiences(setExperiences)
+  }, []); 
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -132,6 +99,7 @@ const Experience = () => {
             <ExperienceCard
               key={`experience-${index}`}
               experience={experience}
+              index = {index}
             />
           ))}
         </VerticalTimeline>
